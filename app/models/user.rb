01ecd09b3b6f32ec49b728ab71b775333650ee04
elmo-24 <coeri124xx@gmail.com>
2021-02-27 18:14:19 +0900

    バリデーションのまとめ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,22 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validates :nickname, presence: true
-  validates :birth_date, presence: true
-  validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-
+  
   with_options presence: true do
+    validates :nickname
+    validates :birth_date
+    validates :password,format:{with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
     with_options format:  {with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/} do
       validates :last_name
       validates :first_name
     end
  
-     with_options format: {with: /\A[ァ-ヶー－]+\z/} do
+    with_options format: {with: /\A[ァ-ヶー－]+\z/} do
       validates :last_name_kana
       validates :first_name_kana
-     end
+    end
  end
 
   has_many :items
